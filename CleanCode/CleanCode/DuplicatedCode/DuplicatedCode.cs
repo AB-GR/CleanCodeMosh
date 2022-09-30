d9f99d0665c22d0b6bb00a043edee2e47160b82e
Abhilash <abhilashgraja@gmail.com>
2022-09-30 08:41:13 +0530

    Removed the GetAdmissionDate method into the Time class and renamed it as parse

--- CleanCode/CleanCode/DuplicatedCode/DuplicatedCode.cs
+++ CleanCode/CleanCode/DuplicatedCode/DuplicatedCode.cs
@@ -3,18 +3,6 @@ using System;
 
 namespace CleanCode.DuplicatedCode
 {
-	class Time
-	{
-		public Time(int hours, int minutes)
-		{
-			Hours = hours;
-			Minutes = minutes;
-		}
-
-		public int Hours { get; }
-		public int Minutes { get; }
-	}
-
     class DuplicatedCode
     {
         public void AdmitGuest(string name, string admissionDateTime)
@@ -22,7 +10,7 @@ namespace CleanCode.DuplicatedCode
 			// Some logic 
 			// ...
 
-			var time = GetAdmissionTime(admissionDateTime);
+			var time = Time.Parse(admissionDateTime);
 
 			// Some more logic 
 			// ...
@@ -37,7 +25,7 @@ namespace CleanCode.DuplicatedCode
 			// Some logic 
 			// ...
 
-			var time = GetAdmissionTime(admissionDateTime);
+			var time = Time.Parse(admissionDateTime);
 
 			// Some more logic 
 			// ...
@@ -45,29 +33,6 @@ namespace CleanCode.DuplicatedCode
 			{
 
 			}
-		}
-
-		private static Time GetAdmissionTime(string admissionDateTime)
-		{
-			int hours;
-			int minutes;
-			if (!string.IsNullOrWhiteSpace(admissionDateTime))
-			{
-				if (int.TryParse(admissionDateTime.Replace(":", ""), out int time))
-				{
-					hours = time / 100;
-					minutes = time % 100;
-				}
-				else
-				{
-					throw new ArgumentException("admissionDateTime");
-				}
-
-			}
-			else
-				throw new ArgumentNullException("admissionDateTime");
-
-			return new Time(hours, minutes);
 		}
 	}
 }
